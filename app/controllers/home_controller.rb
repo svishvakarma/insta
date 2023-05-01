b7ecb883c49bb1ff60e6cc2d6e6706afe61b4e20
sumit <svishvakarma@bestpeers.com>
2023-05-01 17:03:21 +0530

    added home controller and views

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    #todo: show posts by following users here
+    # @posts = Post.joins("INNER JOIN follows ON posts.user_id = follows.followable_id")
+      # .where("follows.follower_id = ?", current_user.id).order('posts.updated_at').reverse_order
+
+    #show profile
+    @post = Post.all
+    @profile = User.select(:handle, :name, :bio, 'profiles.id').joins(:profile).find_by('users.id' => current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#index', as: 'home'
   resources :posts, except: [:index]
 end
