617698c421bdcaeda8471528984bad7bfc2449c8
sumit <svishvakarma@bestpeers.com>
2023-05-01 16:24:31 +0530

    added post model

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  validates :caption, presence: true
+  
+  belongs_to :user
+  
+  # has_many :comments
+  # has_many :likes
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_103506) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_105312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
