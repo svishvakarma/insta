5bca2c5acad1a759e86d6d727b0acb01f822a6f9
sumit <svishvakarma@bestpeers.com>
2023-05-01 17:12:21 +0530

    added like controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'home#index', as: 'home'
   resources :posts, except: [:index]
-  
+
   #profile
   get '/view/:id', to: 'profile#show', as: 'view_profile'
   get '/profile/edit', to: 'profile#edit'
   patch '/profile/update', to: 'profile#update'
   get '/view_all', to: 'profile#show_all'
+
+  
+   #likes
+   post '/like/:post_id', to: 'likes#create', as: 'like'
+   delete '/unlike/:post_id', to: 'likes#destroy', as: 'unlike'
 end
