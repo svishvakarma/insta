774ac4a403d9a6ed44123a0c0ea4c50b7c648a7d
sumit <svishvakarma@bestpeers.com>
2023-05-01 17:16:17 +0530

    added routes for comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(text: params[:comment][:text], post_id: params[:post_id], user_id: current_user.id)
+    respond_to do |format|
+      format.js
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'home#index', as: 'home'
   resources :posts, except: [:index]
-
+  resources :comments, only: [:create, :destroy, :show]
   #profile
   get '/view/:id', to: 'profile#show', as: 'view_profile'
   get '/profile/edit', to: 'profile#edit'
   patch '/profile/update', to: 'profile#update'
   get '/view_all', to: 'profile#show_all'
 
-  
+
    #likes
    post '/like/:post_id', to: 'likes#create', as: 'like'
    delete '/unlike/:post_id', to: 'likes#destroy', as: 'unlike'
